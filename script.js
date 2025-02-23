1d2a3700b21aa151d4f20334459d2873c995c8fd
Tomáš Sedlák <sedlakt@students.zcu.cz>
2025-02-23 13:52:26 +0100

    editing menu and scroll to top button

--- script.js
+++ script.js
@@ -5,28 +5,27 @@ const toggleMenu = () => {
 
 
 // button show
-const button = document.querySelector('.btn_to_top');
-const displayButton = () => {
+const btn = document.querySelector('.btn_to_top');
+const showButton = () => {
     window.addEventListener('scroll', () => {
         console.log(window.scrollY);
-        if (window.scrollY > 100) {
-            button.style.display = "block";
+        if (window.scrollY > 200) {
+            btn.style.display = "block";
         } else {
-            button.style.display = "none";
+            btn.style.display = "none";
         }
     });
 };
 
 // scroll to top
 const scrollToTop = () => {
-    button.addEventListener("click", () => {
+    btn.addEventListener("click", () => {
         window.scroll({
             top: 0,
-            left: 0,
-            behavior: 'smooth'
+            left: 0
         }); 
         console.log(event);
     });
 };
-displayButton();
+showButton();
 scrollToTop();
