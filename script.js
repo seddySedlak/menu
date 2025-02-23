d5adac5f2641471a97e6b9a5ca3d5af15971cd9b
Tomáš Sedlák <sedlakt@students.zcu.cz>
2025-02-23 13:24:50 +0100

    adding scroll to top button

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// hamburder menu
 const toggleMenu = () => {
     document.body.classList.toggle("open");
 };
+
+
+// button show
+const button = document.querySelector('.btn_to_top');
+const displayButton = () => {
+    window.addEventListener('scroll', () => {
+        console.log(window.scrollY);
+        if (window.scrollY > 100) {
+            button.style.display = "block";
+        } else {
+            button.style.display = "none";
+        }
+    });
+};
+
+// scroll to top
+const scrollToTop = () => {
+    button.addEventListener("click", () => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        }); 
+        console.log(event);
+    });
+};
+displayButton();
+scrollToTop();
